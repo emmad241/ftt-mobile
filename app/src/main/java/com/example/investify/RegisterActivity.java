07e8d95c5d1e79ec487b76959c298128a1541e73
Ali Adriaansen <alisonadriaansen@gmail.com>
2022-10-11 22:45:46 +0100

    Started connecting to firebase. Will pick back up on registration page.

--- app/src/main/java/com/example/investify/RegisterActivity.java
+++ app/src/main/java/com/example/investify/RegisterActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 }
