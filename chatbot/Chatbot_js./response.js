62da3eb636833731e455f7fa257b8051a86fc65d
MengJie84 <114220572+MengJie84@users.noreply.github.com>
2022-11-23 21:51:54 +0000

    Add files via upload

---
+++ chatbot/Chatbot_js./response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hi: "Hi there",
+    hey: "Hey! What's Up",
+    thanks: "Any time!",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
